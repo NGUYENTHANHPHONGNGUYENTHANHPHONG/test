f0fc99fc05e9b9099b3f99515f0b22df9207ddc3
NGUYENTHANHPHONGNGUYENTHANHPHONG <119864579+NGUYENTHANHPHONGNGUYENTHANHPHONG@users.noreply.github.com>
2023-03-14 12:53:32 +0700

    lan1
    lan1

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Javacript for img slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".img-slide");
+
+var sliderNav = function (manual) {
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+};
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
+// End Javacript for img slider navigation
+
+// Responsive navbar
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const close = document.getElementById("close-bar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
